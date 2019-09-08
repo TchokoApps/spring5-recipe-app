1c29aae4bb00e6179bcaac53a5e7521bbc1f6ef1
TchokoApps <tchokoapps@gmail.com>
2019-09-08 14:53:05 +0200

    update

--- src/main/java/com/tchokoapps/springboot/spring5recipeapp/entities/Difficulty.java
+++ src/main/java/com/tchokoapps/springboot/spring5recipeapp/entities/Difficulty.java
@@ -1,4 +1,5 @@
 package com.tchokoapps.springboot.spring5recipeapp.entities;
 
 public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
 }
--- src/main/java/com/tchokoapps/springboot/spring5recipeapp/entities/Recipe.java
+++ src/main/java/com/tchokoapps/springboot/spring5recipeapp/entities/Recipe.java
@@ -16,6 +16,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -26,6 +28,11 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -113,5 +120,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
