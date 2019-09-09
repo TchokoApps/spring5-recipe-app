6c0757022cd5735e2a6d2c4b1c071370bfaac799
TchokoApps <tchokoapps@gmail.com>
2019-09-09 23:03:45 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring5-recipe-app
+Project Lombok
+
+In order to use it correct please install the intellij pluging of this project.
--- src/main/java/com/tchokoapps/springboot/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/tchokoapps/springboot/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.tchokoapps.springboot.spring5recipeapp.services;
 
 import com.tchokoapps.springboot.spring5recipeapp.repositories.RecipeRepository;
 import com.tchokoapps.springboot.spring5recipeapp.entities.Recipe;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am a test");
         HashSet<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
